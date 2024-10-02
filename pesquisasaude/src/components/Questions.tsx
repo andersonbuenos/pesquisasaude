707c22aa160a66d909089dc24607a5a48b3a3e98
andersonbuenos <andersonbuenos@live.com>
2024-10-02 04:48:24 -0300

    edição do forms

--- pesquisasaude/src/components/Questions.tsx
+++ pesquisasaude/src/components/Questions.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import './Questions.css'; // Importando o arquivo de estilos CSS
+import './Questions.css';
 
-// Definindo a interface para formData
+// Defina a estrutura do objeto de dados do formulário
 interface FormData {
   vigilanciaEpidemiologica: string;
   leitosUTI: string;
@@ -36,7 +36,6 @@ interface FormData {
 }
 
 const Questions = () => {
-  // Usando a interface FormData para tipar o estado
   const [formData, setFormData] = useState<FormData>({
     vigilanciaEpidemiologica: "",
     leitosUTI: "",
@@ -84,218 +83,116 @@ const Questions = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   const questions = [
-    // Suas perguntas aqui...
-    {
-      question: "Esta unidade faz parte da Vigilância Epidemiológica?",
-      options: ["Sim", "Não"],
-      key: "vigilanciaEpidemiologica",
-    },
-    {
-      question: "Possui leitos de UTI?",
-      options: [
-        "Somente UTI Adulto",
-        "Somente UTI Infantil",
-        "UTI Adulto e Pediátrico",
-        "Não possui UTI",
-      ],
-      key: "leitosUTI",
-    },
-    // Continue adicionando suas perguntas
-    {
-      question: "Quais observações você gostaria de adicionar?",
-      type: "text",
-      key: "observacoesA",
-    },
-    {
-      question: "Qual o nome do responsável pela Vigilância Epidemiológica?",
-      type: "text",
-      key: "nomeVigEpi",
-    },
-    {
-      question: "Qual o contato da Vigilância Epidemiológica?",
-      type: "text",
-      key: "contatoVigEpi",
-    },
-    {
-      question: "A unidade possui RT da Rede Frio?",
-      options: ["Sim", "Não"],
-      key: "rtRedeFrio",
-    },
-    {
-      question: "Qual o e-mail do RT?",
-      type: "text",
-      key: "emailRT",
-    },
-    {
-      question: "Qual o telefone do RT?",
-      type: "text",
-      key: "telefoneRT",
-    },
-    {
-      question: "Quais comentários adicionais você gostaria de fazer?",
-      type: "text",
-      key: "comentariosB",
-    },
-    {
-      question: "A unidade pertence à Rede Frio Municipal?",
-      options: ["Sim", "Não"],
-      key: "redeFrioMunicipal",
-    },
-    {
-      question: "Que tipos de soros a unidade possui?",
-      options: [
-        "Anti-botrópico",
-        "Anti-crotálico",
-        "Anti-elapídico",
-        "Anti-laquético",
-        "Anti-botrópico/crotálico",
-        "Anti-botrópico/laquético",
-        "Anti-aracnídico",
-        "Anti-escorpiônico",
-        "Anti-loxoscélico",
-        "Anti-lonômico",
-      ],
-      key: "tipoSoros",
-    },
-    {
-      question: "Como está o armazenamento de soros?",
-      options: ["Adequado", "Inadequado"],
-      key: "armazenamento",
-    },
-    {
-      question: "Como está a manutenção da estrutura?",
-      options: ["Adequado", "Inadequado"],
-      key: "manutencaoEstrutura",
-    },
-    {
-      question: "Quais comentários sobre a manutenção você gostaria de adicionar?",
-      type: "text",
-      key: "comentariosManutencao",
-    },
-    {
-      question: "A unidade possui equipamentos de conservação?",
-      options: ["Sim", "Não"],
-      key: "equipamentoConservacao",
-    },
-    {
-      question: "Como está o armazenamento de conservação?",
-      options: ["Adequado", "Inadequado"],
-      key: "armazenamentoConservacao",
-    },
-    {
-      question: "Quais comentários sobre a conservação você gostaria de adicionar?",
-      type: "text",
-      key: "comentariosConservacao",
-    },
-    {
-      question: "Como está o controle de estoque?",
-      options: ["Adequado", "Inadequado"],
-      key: "controleEstoque",
-    },
-    {
-      question: "Houve intercorrências?",
-      options: ["Sim", "Não"],
-      key: "intercorrencia",
-    },
-    {
-      question: "Os registros de notificações estão adequados?",
-      options: ["Sim", "Não"],
-      key: "registrosNotificacoes",
-    },
-    {
-      question: "Como está o transporte de soros?",
-      options: ["Adequado", "Inadequado"],
-      key: "transporte",
-    },
-    {
-      question: "A equipe recebeu capacitação?",
-      options: ["Sim", "Não"],
-      key: "capacitacaoEquipe",
-    },
-    {
-      question: "O PGRSS está implementado?",
-      options: ["Sim", "Não"],
-      key: "pgrss",
-    },
-    {
-      question: "A unidade possui licença sanitária?",
-      options: ["Sim", "Não"],
-      key: "licencaSanitaria",
-    },
-    {
-      question: "Qual o status do RT?",
-      options: ["Ativo", "Inativo"],
-      key: "rtStatus",
-    },
-    {
-      question: "Qual o nome do substituto do RT?",
-      type: "text",
-      key: "substitutoRT.nome",
-    },
-    {
-      question: "Qual o telefone do substituto do RT?",
-      type: "text",
-      key: "substitutoRT.telefone",
-    },
-    {
-      question: "Qual o e-mail do substituto do RT?",
-      type: "text",
-      key: "substitutoRT.email",
-    },
+    { question: "Esta unidade faz parte da Vigilância Epidemiológica?", options: ["Sim", "Não"], key: "vigilanciaEpidemiologica", isRequired: true },
+    { question: "Possui leitos de UTI?", options: ["Somente UTI Adulto", "Somente UTI Infantil", "UTI Adulto e Pediátrico", "Não possui UTI"], key: "leitosUTI", isRequired: true },
+    { question: "Quais observações você gostaria de adicionar?", type: "text", key: "observacoesA" },
+    { question: "Qual o nome do RT da Vigilância Epidemiológica?", type: "text", key: "nomeVigEpi", isRequired: true },
+    { question: "Qual o contato do RT da Vigilância Epidemiológica?", type: "text", key: "contatoVigEpi", isRequired: true },
+    { question: "Qual o nome do RT da Rede de Frio?", type: "text", key: "rtRedeFrio", isRequired: true },
+    { question: "Qual o e-mail do RT da Rede de Frio?", type: "text", key: "emailRT", isRequired: true },
+    { question: "Qual o telefone do RT da Rede de Frio?", type: "text", key: "telefoneRT", isRequired: true },
+    { question: "Comentários adicionais sobre o RT da Rede de Frio:", type: "text", key: "comentariosB" },
+    { question: "A unidade possui rede de frio municipal?", options: ["Sim", "Não"], key: "redeFrioMunicipal", isRequired: true },
+    { question: "Qual o tipo de soros disponíveis?", options: ["Anti-botrópico", "Anti-crotálico", "Anti-elapídico", "Anti-laquético"], key: "tipoSoros", isRequired: true },
+    { question: "Como é feito o armazenamento?", options: ["Adequado", "Inadequado"], key: "armazenamento", isRequired: true },
+    { question: "Manutenção da estrutura é feita regularmente?", options: ["Sim", "Não"], key: "manutencaoEstrutura", isRequired: true },
+    { question: "Comentários sobre a manutenção da estrutura:", type: "text", key: "comentariosManutencao" },
+    { question: "Equipamentos de conservação estão em boas condições?", options: ["Sim", "Não"], key: "equipamentoConservacao", isRequired: true },
+    { question: "Como é feito o armazenamento da conservação?", options: ["Adequado", "Inadequado"], key: "armazenamentoConservacao", isRequired: true },
+    { question: "Comentários sobre a conservação:", type: "text", key: "comentariosConservacao" },
+    { question: "Controle de estoque é feito regularmente?", options: ["Sim", "Não"], key: "controleEstoque", isRequired: true },
+    { question: "Como a unidade lida com intercorrências?", options: ["Protocolo definido", "Não há protocolo"], key: "intercorrencia", isRequired: true },
+    { question: "Registros e notificações são realizados corretamente?", options: ["Sim", "Não"], key: "registrosNotificacoes", isRequired: true },
+    { question: "O transporte da vacina está adequado?", options: ["Sim", "Não"], key: "transporte", isRequired: true },
+    { question: "A equipe foi capacitada para a conservação da vacina?", options: ["Sim", "Não"], key: "capacitacaoEquipe", isRequired: true },
+    { question: "O PGRSS está implementado na unidade?", options: ["Sim", "Não"], key: "pgrss", isRequired: true },
+    { question: "A unidade possui licença sanitária?", options: ["Sim", "Não"], key: "licencaSanitaria", isRequired: true },
+    { question: "Qual é o status do RT da unidade?", options: ["Ativo", "Substituto"], key: "rtStatus", isRequired: true },
+    { question: "Nome do substituto do RT:", type: "text", key: "substitutoRT.nome" },
+    { question: "Telefone do substituto do RT:", type: "text", key: "substitutoRT.telefone" },
+    { question: "E-mail do substituto do RT:", type: "text", key: "substitutoRT.email" },
+    // Outras perguntas...
   ];
 
-  const handleChange = (key: string, value: string) => {
-    setFormData((prevData) => ({ ...prevData, [key]: value }));
-  };
-
   const handleNextQuestion = () => {
-    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    const currentKey = questions[currentQuestionIndex].key as keyof FormData; // Asserção de tipo
+  
+    // Verifique se o formData contém a chave e se o valor não está vazio
+    if (formData[currentKey] !== undefined && formData[currentKey] !== '') {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    }
   };
+  
 
   const handleSkipQuestion = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
   };
+
+  const handleOptionChange = (key: string, value: string) => {
+    // Atualiza o estado do formData ao selecionar uma opção
+    setFormData((prevData) => ({
+      ...prevData,
+      [key]: value,
+    }));
+  };
+
+  const handleTextChange = (key: string, value: string) => {
+    // Atualiza o estado do formData ao digitar um texto
+    setFormData((prevData) => ({
+      ...prevData,
+      [key]: value,
+    }));
+  };
+
+  const TextInput = ({ value, onChange }: { value: string; onChange: (value: string) => void }) => (
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+  
+  // Uso no JSX
+  type FormDataKeys = keyof FormData; // Define as chaves do FormData
+
+  const currentKey: FormDataKeys = questions[currentQuestionIndex].key as FormDataKeys; // Assegura que currentKey é um FormDataKey
+  const value = formData[currentKey] as string; 
 
   return (
     <div className="questions-container">
       {currentQuestionIndex < questions.length ? (
         <div className="question-card">
           <h3 className="question-title">{questions[currentQuestionIndex]?.question}</h3>
-          {questions[currentQuestionIndex]?.type === "text" ? (
-            <input
-              type="text"
-              className="text-input"
-              onChange={(e) =>
-                handleChange(questions[currentQuestionIndex].key, e.target.value)
-              }
-            />
+          {questions[currentQuestionIndex].options ? (
+            <div>
+              {questions[currentQuestionIndex].options.map((option) => (
+                <label class="option-label" key={option}>
+                  <input
+                    type="radio"
+                    name={questions[currentQuestionIndex].key}
+                    value={option}
+                    onChange={() => handleOptionChange(questions[currentQuestionIndex].key, option)}
+                  />
+                  {option}
+                </label>
+              ))}
+            </div>
           ) : (
-            questions[currentQuestionIndex]?.options?.map((option, index) => (
-              <div className="option-container" key={index}>
-                <input
-                  type="radio"
-                  name={questions[currentQuestionIndex].key}
-                  value={option}
-                  onChange={() =>
-                    handleChange(questions[currentQuestionIndex].key, option)
-                  }
-                />
-                <label className="option-label">{option}</label>
-              </div>
-            ))
+            <input
+            type="text"
+            className="text-input" // Classe CSS adicionada para estilização
+            value={value} // Use o valor do formData
+            onChange={(e) => handleTextChange(currentKey, e.target.value)} // Atualize com o valor do formData
+          />
           )}
-          <div className="button-container">
-            <button className="next-button" onClick={handleNextQuestion}>
-              Próxima Pergunta
-            </button>
-            <button className="skip-button" onClick={handleSkipQuestion}>
-              Pular Pergunta
-            </button>
+          <div className="navigation-buttons">
+            <button onClick={handleSkipQuestion}></button>
+            <button class="skip-button" onClick={handleNextQuestion}>Próximo</button>
           </div>
         </div>
       ) : (
-        <div className="thank-you-message">
-          <h3>Obrigado por responder às perguntas!</h3>
+        <div className="end-message">
+          <h3>Obrigado por responder!</h3>
         </div>
       )}
     </div>
